6c4ee28a3d2e743f847eecedb76eb9b8d862c674
Foursqweez <jeerasak.code@gmail.com>
2025-01-29 14:35:12 +0700

    Create user page and delete user feature

---
+++ user.js
@@ -0,0 +1,45 @@
+const BASE_URL = "http://localhost:8000";
+
+window.onload = async () => {
+  console.log("on load");
+  await loadData();
+};
+
+const loadData = async () => {
+  // 1. Load all the user from the API
+  const response = await axios.get(`${BASE_URL}/users`);
+  console.log(response.data);
+
+  // 2. Put the data into the html
+  const userDom = document.getElementById("user");
+
+  let htmlData = "<div>";
+  for (let i = 0; i < response.data.length; i++) {
+    let user = response.data[i];
+    htmlData += `
+    <div>
+   ${user.id} ${user.firstname} ${user.lastname}
+    <button>Edit</button>
+    <button class='delete' data-id='${user.id}'>Delete</button>
+    </div>
+    
+    `;
+  }
+  htmlData += "</div>";
+  userDom.innerHTML = htmlData;
+
+  // button click event
+  const deleteDOMs = document.getElementsByClassName("delete");
+
+  for (let i = 0; i < deleteDOMs.length; i++) {
+    deleteDOMs[i].addEventListener("click", async (event) => {
+      const userId = event.target.dataset.id;
+      try {
+        await axios.delete(`${BASE_URL}/user/${userId}`);
+        loadData(); // recursive function
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }
+};
